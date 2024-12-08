bf88a1ca80b7af719644d1cb2d40ff647d1854c4
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-08 23:38:43 +0900

    Update Admin.java

--- Project_OOP/src/Models/Admin.java
+++ Project_OOP/src/Models/Admin.java
@@ -1,9 +1,8 @@
 package Models;
 
 public class Admin extends User {
-
-	public Admin(String username, String password) {
-		super(username, password);
-	}
-
+	
+    public Admin(String username, String password, String userId, String email, String nameLast, String nameFirst) {
+        super(username, password, userId, email, nameLast, nameFirst);
+    }
 }
