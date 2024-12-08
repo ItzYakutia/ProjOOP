fab04170f46a24969a411a0427451573fc0df732
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-08 23:40:18 +0900

    Create FinanceManager.java

---
+++ Project_OOP/src/Models/FinanceManager.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class FinanceManager extends User {
+	
+    public FinanceManager(String username, String password, String userId, String email, String nameLast, String nameFirst) {
+        super(username, password, userId, email, nameLast, nameFirst);
+    }
+}
