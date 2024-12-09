d738dbac9735b79fd96383fc1380fc93e39298c5
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-10 00:13:04 +0500

    Create ResearcherView.java

---
+++ Project_OOP/src/Views/ResearcherView.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class ResearcherView {
+    public void displayResearchPapers(List<ResearchPaper> papers) {
+        System.out.println("Research Papers:");
+        for (ResearchPaper paper : papers) {
+            System.out.println("- " + paper.getTitle());
+        }
+    }
+
+    public void displayHIndex(int hIndex) {
+        System.out.println("H-Index: " + hIndex);
+    }
+}
