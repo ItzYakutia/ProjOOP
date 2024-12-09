0b36001514acca334a4b82faaab3554fb0ecb070
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-10 00:05:50 +0500

    Create Rectorview.java

---
+++ Project_OOP/src/Views/Rectorview.java
@@ -0,0 +1,9 @@
+public class RectorView {
+    public void displayUniversityBudget(double budget) {
+        System.out.println("Current University Budget: $" + budget);
+    }
+
+    public void displayMessage(String message) {
+        System.out.println("Message: " + message);
+    }
+}
