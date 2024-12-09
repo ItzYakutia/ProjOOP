101be2dc7715e83bfbdc1af7ecebc9d2592fa610
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-10 00:14:58 +0500

    Update ResearchProject.java

--- Project_OOP/src/Models/ResearchProject.java
+++ Project_OOP/src/Models/ResearchProject.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ResearchProject {
     private String topic;
-    private List<ResearchPaper> publishedPapers;
     private List<Researcher> participants;
 
     public ResearchProject(String topic) {
         this.topic = topic;
-        this.publishedPapers = new ArrayList<>();
         this.participants = new ArrayList<>();
     }
 
@@ -17,47 +14,14 @@ public class ResearchProject {
         return topic;
     }
 
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public List<ResearchPaper> getPublishedPapers() {
-        return publishedPapers;
-    }
-
-    public void addPublishedPaper(ResearchPaper paper) {
-        publishedPapers.add(paper);
-        System.out.println("Added published paper: " + paper.getTitle());
+    public void addParticipant(Researcher researcher) throws InvalidParticipantException {
+        if (!(researcher instanceof Researcher)) {
+            throw new InvalidParticipantException("Only researchers can be participants!");
+        }
+        participants.add(researcher);
     }
 
     public List<Researcher> getParticipants() {
         return participants;
-    }
-
-    public void addParticipant(Researcher researcher) {
-        participants.add(researcher);
-        System.out.println("Added participant: " + researcher.getName());
-    }
-
-    @Override
-    public String toString() {
-        return "ResearchProject{" +
-                "topic='" + topic + '\'' +
-                ", publishedPapers=" + publishedPapers.size() +
-                ", participants=" + participants.size() +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ResearchProject)) return false;
-        ResearchProject that = (ResearchProject) o;
-        return Objects.equals(topic, that.topic);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(topic);
     }
 }
