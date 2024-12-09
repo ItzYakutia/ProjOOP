757639581d6cfd74a887b9362e8d0b39ab954ef7
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-10 00:19:08 +0500

    Create DeanController.java

---
+++ Project_OOP/src/Controllers/DeanController.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class DeanController {
+    private Dean dean;
+    private DeanView deanView;
+
+    public DeanController(Dean dean, DeanView deanView) {
+        this.dean = dean;
+        this.deanView = deanView;
+    }
+
+    public void manageFaculty(String department, boolean action) {
+        dean.manageFaculty(department, action);
+        deanView.displayMessage("Faculty " + department + " has been " + (action ? "activated" : "deactivated"));
+    }
+
+    public void approveFunding(String researchId, double amount) {
+        try {
+            dean.approveResearchFunding(researchId, amount);
+            deanView.displayFundingApproval(researchId, amount);
+        } catch (InsufficientFundsException e) {
+            deanView.displayMessage(e.getMessage());
+        }
+    }
+
+    public void reviewCourseProposals(List<Course> proposals) {
+        dean.reviewCourseProposals(proposals);
+        deanView.displayCourseProposals(proposals);
+    }
+}
