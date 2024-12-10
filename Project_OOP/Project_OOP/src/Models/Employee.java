f8ad7aad153f6f2f1f395f9423b2fad8ac78f8dd
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-10 18:52:52 +0900

    Delete Project_OOP/Project_OOP directory

--- Project_OOP/Project_OOP/src/Models/Employee.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public abstract class Employee extends User{
-
-	public Employee(String username, String password, String userId, String email, String nameLast, String nameFirst) {
-		super(username, password, userId, email, nameLast, nameFirst);
-	}
-
-}
