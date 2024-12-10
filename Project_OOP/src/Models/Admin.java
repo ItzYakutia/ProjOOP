a7bcdb2547d28178ce7729bf151cbfd268ae8138
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-10 16:06:16 +0900

    Delete Project_OOP/src directory

--- Project_OOP/src/Models/Admin.java
+++
@@ -1,8 +0,0 @@
-package Models;
-
-public class Admin extends User {
-	
-    public Admin(String username, String password, String userId, String email, String nameLast, String nameFirst) {
-        super(username, password, userId, email, nameLast, nameFirst);
-    }
-}
