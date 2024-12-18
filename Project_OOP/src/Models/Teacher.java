777a953acce0c1fa3fe46bbf2678515b6cd1cfa0
adiyetyer <157557519+adiyetyer@users.noreply.github.com>
2024-12-19 01:21:06 +0500

    Update Teacher.java
    equals

--- Project_OOP/src/Models/Teacher.java
+++ Project_OOP/src/Models/Teacher.java
@@ -1,22 +1,24 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.List;
-import Models.Title;
+import java.util.Objects;
 
 public class Teacher extends Employee {
 
-    private Title title; // Звание преподавателя (например, Professor, Lecturer)
+    private Title title; // Звание преподавателя (например, Lecturer, Professor)
     private List<Course> courses; // Список курсов, которые ведёт преподаватель
-    private List<Message> complaints; // Список жалоб на студентов или другие вопросы
+    private List<String> complaints; // Жалобы на студентов, отправленные декану
 
     public Teacher(String username, String password, String userId, String nameFirst, String nameLast, String email,
-                   Title title, List<Course> courses, List<Message> complaints) {
-        super(username, password, userId, nameFirst, nameLast, email);
+                   int workingYears, Title title, List<Course> courses, List<String> complaints) {
+        super(username, password, userId, nameFirst, nameLast, email, workingYears);
         this.title = title;
-        this.courses = courses;
-        this.complaints = complaints;
+        this.courses = courses != null ? courses : new ArrayList<>();
+        this.complaints = complaints != null ? complaints : new ArrayList<>();
     }
 
+    // Геттеры и сеттеры
     public Title getTitle() {
         return title;
     }
@@ -29,28 +31,47 @@ public class Teacher extends Employee {
         return courses;
     }
 
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
+    public void addCourse(Course course) {
+        courses.add(course);
     }
 
-    public List<Message> getComplaints() {
+    public void removeCourse(Course course) {
+        courses.remove(course);
+    }
+
+    public List<String> getComplaints() {
         return complaints;
     }
 
-    public void setComplaints(List<Message> complaints) {
-        this.complaints = complaints;
+    public void addComplaint(String complaint) {
+        complaints.add(complaint);
     }
 
+    // Реализация equals для сравнения преподавателей по userId
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return getUserId().equals(teacher.getUserId());
+    }
+
+    // Реализация hashCode для использования в хэш-таблицах
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId());
+    }
+
+    // Реализация toString для удобного вывода информации о преподавателе
     @Override
     public String toString() {
         return "Teacher{" +
-                "username='" + getUsername() + '\'' +
-                ", userId='" + getUserId() + '\'' +
+                "userId='" + getUserId() + '\'' +
                 ", name='" + getNameFirst() + " " + getNameLast() + '\'' +
-                ", email='" + getEmail() + '\'' +
-                ", title='" + title + '\'' +
-                ", courses=" + courses +
-                ", complaints=" + complaints +
+                ", title=" + title +
+                ", workingYears=" + getWorkingYears() +
+                ", courses=" + courses.size() +
+                ", complaints=" + complaints.size() +
                 '}';
     }
 }
