f29aa56efb90b25fec9b58c2b33d0a677f0cf258
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-20 17:42:43 +0500

    Create News.java

---
+++ ProjOOP-main/Project_OOP/src/Models/News.java
@@ -0,0 +1,16 @@
+package Models;
+
+public class News {
+    private String title;
+    private String content;
+
+    public News(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "News{" + "title='" + title + '\'' + ", content='" + content + '\'' + '}';
+    }
+}
