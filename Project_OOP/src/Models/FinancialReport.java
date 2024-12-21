1639f689b0a3dce9c9736bb27e7e846b29a1ec41
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:52:19 +0900

    Update FinancialReport.java

--- Project_OOP/src/Models/FinancialReport.java
+++ Project_OOP/src/Models/FinancialReport.java
@@ -1,5 +1,46 @@
 package Models;
 
 public class FinancialReport {
+    private double totalIncome;
+    private double totalExpenses;
+    private double netIncome;
 
+    public FinancialReport(double totalIncome, double totalExpenses, double netIncome) {
+        this.totalIncome = totalIncome;
+        this.totalExpenses = totalExpenses;
+        this.netIncome = netIncome;
+    }
+
+    public double getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(double totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public double getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public void setTotalExpenses(double totalExpenses) {
+        this.totalExpenses = totalExpenses;
+    }
+
+    public double getNetIncome() {
+        return netIncome;
+    }
+
+    public void setNetIncome(double netIncome) {
+        this.netIncome = netIncome;
+    }
+
+    @Override
+    public String toString() {
+        return "FinancialReport{" +
+                "totalIncome=" + totalIncome +
+                ", totalExpenses=" + totalExpenses +
+                ", netIncome=" + netIncome +
+                '}';
+    }
 }
