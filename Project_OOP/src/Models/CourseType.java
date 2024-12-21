f038ee04b41d1c663dcbf0bbfd306a5f1bb00f90
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 06:26:46 +0900

    Update CourseType.java

--- Project_OOP/src/Models/CourseType.java
+++ Project_OOP/src/Models/CourseType.java
@@ -1,7 +1,22 @@
 package Models;
 
 public enum CourseType {
-    MAJOR,  // Основные курсы
-    MINOR,  // Дополнительные курсы
-    FREE_ELECTIVE // Свободные элективы
+    MAJOR("Major"),
+    MINOR("Minor"),
+    FREE_ELECTIVE("Free elective");
+
+    private final String displayType;
+
+    CourseType(String displayType) {
+        this.displayType = displayType;
+    }
+
+    public String displayType() {
+        return displayType;
+    }
+
+    @Override
+    public String toString() {
+        return displayType;
+    }
 }
