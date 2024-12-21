0a49651b132be218ed5ffd1fc3b77f89a3fd50e1
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:53:33 +0900

    Update Transaction.java

--- Project_OOP/src/Models/Transaction.java
+++ Project_OOP/src/Models/Transaction.java
@@ -1,5 +1,46 @@
 package Models;
 
 public class Transaction {
+    private String transactionId;
+    private String description;
+    private double amount;
 
+    public Transaction(String transactionId, String description, double amount) {
+        this.transactionId = transactionId;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
