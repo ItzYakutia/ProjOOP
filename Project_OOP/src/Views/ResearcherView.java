8db0bd97578763c8b77ba8ea928c0c98b452db1c
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 00:58:25 +0500

    Update ResearcherView.java

--- Project_OOP/src/Views/ResearcherView.java
+++ Project_OOP/src/Views/ResearcherView.java
@@ -1,18 +1,30 @@
 package Views;
 
-import java.util.List;
+import Models.ResearchPaper;
+import Models.ResearchProject;
 
-import Models.*;
+import java.util.List;
 
 public class ResearcherView {
     public void displayResearchPapers(List<ResearchPaper> papers) {
         System.out.println("Research Papers:");
         for (ResearchPaper paper : papers) {
-            System.out.println("- " + paper.getTitle());
+            System.out.println("- " + paper);
         }
     }
 
     public void displayHIndex(int hIndex) {
-        System.out.println("H-Index: " + hIndex);
+        System.out.println("Researcher's H-index: " + hIndex);
+    }
+
+    public void displayProjects(List<ResearchProject> projects) {
+        System.out.println("Research Projects:");
+        for (ResearchProject project : projects) {
+            System.out.println("- " + project.getTopic());
+        }
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
     }
 }
