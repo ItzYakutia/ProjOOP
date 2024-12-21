51f13513587146d671ae24222b7cdbd503fc687f
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:10:13 +0900

    Update Admin.java

--- Project_OOP/src/Models/Admin.java
+++ Project_OOP/src/Models/Admin.java
@@ -1,8 +1,26 @@
 package Models;
 
+import java.util.Objects;
+
 public class Admin extends User {
-	
     public Admin(String username, String password, String userId, String email, String nameLast, String nameFirst) {
         super(username, password, userId, email, nameLast, nameFirst);
+    }
+
+    @Override
+    public void receiveNotification(String message) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Admin admin)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(getUsername(), admin.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getUsername());
     }
 }
