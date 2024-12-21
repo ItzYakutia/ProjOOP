b0ac95b1ff398e8292ad677e5a37c93c2344c146
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:36:18 +0900

    Update Allocation.java

--- Project_OOP/src/Models/Allocation.java
+++ Project_OOP/src/Models/Allocation.java
@@ -1,5 +1,46 @@
 package Models;
 
-public class Allocation {
+import java.util.Date;
 
+public class Allocation {
+    private int departmentId;
+    private double amount;
+
+    public Allocation(int departmentId, double amount) {
+        this.departmentId = departmentId;
+        this.amount = amount;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getAllocationDate() {
+        return allocationDate;
+    }
+
+    public void setAllocationDate(Date allocationDate) {
+        this.allocationDate = allocationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Allocation{" +
+                "departmentId=" + departmentId +
+                ", amount=" + amount +
+                ", allocationDate=" + allocationDate +
+                '}';
+    }
 }
