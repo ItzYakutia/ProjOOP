083569db66cb9c558daa6569cb2f4daebd62bb8b
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 01:06:18 +0500

    Update ResearchProjectView.java

--- Project_OOP/src/Views/ResearchProjectView.java
+++ Project_OOP/src/Views/ResearchProjectView.java
@@ -1,15 +1,30 @@
 package Views;
 
-import java.util.List;
-
-import Models.*;
+import Models.ResearchPaper;
+import Models.ResearchProject;
+import Models.Researcher;
 
 public class ResearchProjectView {
-    public void displayProjectDetails(String topic, List<Researcher> participants) {
-        System.out.println("Project Topic: " + topic);
+
+    public void displayProjectDetails(ResearchProject project) {
+        System.out.println("Research Project Details:");
+        System.out.println("Topic: " + project.getTopic());
+        System.out.println("Supervisor: " + project.getSupervisor().getNameFirst() + " " + project.getSupervisor().getNameLast());
         System.out.println("Participants:");
-        for (Researcher researcher : participants) {
-            System.out.println("- " + researcher.getUsername());
+        for (Researcher participant : project.getParticipants()) {
+            System.out.println("- " + participant.getNameFirst() + " " + participant.getNameLast());
         }
+        System.out.println("Published Papers:");
+        for (ResearchPaper paper : project.getPublishedPapers()) {
+            System.out.println("- " + paper.getTitle());
+        }
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayDiplomaApproval(String projectTitle, Researcher researcher) {
+        System.out.println("Diploma project \"" + projectTitle + "\" approved for Researcher: " + researcher.getNameFirst() + " " + researcher.getNameLast());
     }
 }
