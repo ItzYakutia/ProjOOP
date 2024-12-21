665aeeb165e3a873f493d7d97e4b87a37b5cad7d
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 04:12:57 +0500

    Update RectorController.java

--- Project_OOP/src/Controllers/RectorController.java
+++ Project_OOP/src/Controllers/RectorController.java
@@ -1,7 +1,12 @@
 package Controllers;
 
 import Models.Rector;
+import Models.ResearchPaper;
+import Models.Researcher;
 import Views.RectorView;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class RectorController {
     private Rector rector;
@@ -38,5 +43,19 @@ public class RectorController {
     public void signAgreement(String agreementName) {
         rector.signAgreement(agreementName);
         rectorView.displaySignedAgreement(agreementName);
+    }
+
+    public void printAllResearchPapers(List<Researcher> researchers, Comparator<ResearchPaper> comparator) {
+        rector.printAllResearchPapers(researchers, comparator);
+    }
+
+    public void displayTopCitedResearcherBySchool(List<Researcher> researchers, String schoolName) {
+        Researcher researcher = rector.getTopCitedResearcherBySchool(researchers, schoolName);
+        rectorView.displayTopCitedResearcher(researcher);
+    }
+
+    public void displayTopCitedResearcherOfYear(List<Researcher> researchers, int year) {
+        Researcher researcher = rector.getTopCitedResearcherOfYear(researchers, year);
+        rectorView.displayTopCitedResearcher(researcher);
     }
 }
