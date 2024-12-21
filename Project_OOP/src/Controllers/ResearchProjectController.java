2bb6da4c264f9d2495bbd7e1c4650b57fe249d6f
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 01:06:26 +0500

    Update ResearchProjectController.java

--- Project_OOP/src/Controllers/ResearchProjectController.java
+++ Project_OOP/src/Controllers/ResearchProjectController.java
@@ -1,26 +1,43 @@
 package Controllers;
 
-import Models.*;
-import Views.*;
+import Models.ResearchPaper;
+import Models.ResearchProject;
+import Models.Researcher;
+import Views.ResearchProjectView;
 
 public class ResearchProjectController {
     private ResearchProject project;
-    private ResearchProjectView projectView;
+    private ResearchProjectView view;
 
-    public ResearchProjectController(ResearchProject project, ResearchProjectView projectView) {
+    public ResearchProjectController(ResearchProject project, ResearchProjectView view) {
         this.project = project;
-        this.projectView = projectView;
+        this.view = view;
+    }
+
+    public void showProjectDetails() {
+        view.displayProjectDetails(project);
+    }
+
+    public void addResearchPaper(ResearchPaper paper) {
+        project.addResearchPaper(paper);
+        view.displayMessage("Research paper added to the project: " + paper.getTitle());
     }
 
     public void addParticipant(Researcher researcher) {
         try {
             project.addParticipant(researcher);
-        } catch (InvalidParticipantException e) {
-            System.out.println(e.getMessage());
+            view.displayMessage("Participant added to the project: " + researcher.getNameFirst());
+        } catch (IllegalArgumentException e) {
+            view.displayMessage(e.getMessage());
         }
     }
 
-    public void displayProject() {
-        projectView.displayProjectDetails(project.getTopic(), project.getParticipants());
+    public void approveDiplomaProject(String projectTitle, Researcher researcher) {
+        try {
+            project.approveDiplomaProject(projectTitle, researcher);
+            view.displayDiplomaApproval(projectTitle, researcher);
+        } catch (IllegalArgumentException e) {
+            view.displayMessage(e.getMessage());
+        }
     }
 }
