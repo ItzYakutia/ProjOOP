4aba2805f866fa18822236ca9bae1c31feac0fbe
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:40:37 +0900

    Update InvalidCourseException.java

--- Project_OOP/src/Models/InvalidCourseException.java
+++ Project_OOP/src/Models/InvalidCourseException.java
@@ -1,7 +1,9 @@
-package Exceptions;
+package Models;
 
 public class InvalidCourseException extends Exception {
-    public InvalidCourseException(String message) {
+    private static final long serialVersionUID = 1L;
+
+	public InvalidCourseException(String message) {
         super(message);
     }
 }
