4569cc291508163bdbaad720a9b36ec25ae6ece6
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:38:55 +0900

    Update ExpenseRequest.java

--- Project_OOP/src/Models/ExpenseRequest.java
+++ Project_OOP/src/Models/ExpenseRequest.java
@@ -1,5 +1,57 @@
 package Models;
 
 public class ExpenseRequest {
+    private int requestId;
+    private String description;
+    private double amount;
+    private boolean approved;
 
+    public ExpenseRequest(int requestId, String description, double amount, boolean approved) {
+        this.requestId = requestId;
+        this.description = description;
+        this.amount = amount;
+        this.approved = approved;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseRequest{" +
+                "requestId=" + requestId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", approved=" + approved +
+                '}';
+    }
 }
