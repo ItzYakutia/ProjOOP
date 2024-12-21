b53123817953bbaae85f2352c1079343162d2954
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 07:36:02 +0900

    Update BudgetReport.java

--- Project_OOP/src/Models/BudgetReport.java
+++ Project_OOP/src/Models/BudgetReport.java
@@ -1,5 +1,45 @@
 package Models;
 
 public class BudgetReport {
+    private double totalBudget;
+    private double utilizedBudget;
+    private double remainingBudget;
 
+    public BudgetReport(double totalBudget, double utilizedBudget, double remainingBudget) {
+        this.totalBudget = totalBudget;
+        this.utilizedBudget = utilizedBudget;
+        this.remainingBudget = remainingBudget;
+    }
+
+    public double getTotalBudget() {
+        return totalBudget;
+    }
+
+    public void setTotalBudget(double totalBudget) {
+        this.totalBudget = totalBudget;
+    }
+
+    public double getUtilizedBudget() {
+        return utilizedBudget;
+    }
+
+    public void setUtilizedBudget(double utilizedBudget) {
+        this.utilizedBudget = utilizedBudget;
+    }
+
+    public double getRemainingBudget() {
+        return remainingBudget;
+    }
+
+    public void setRemainingBudget(double remainingBudget) {
+        this.remainingBudget = remainingBudget;
+    }
+
+    @Override
+    public String toString() {
+        return "Total Budget: " + totalBudget +
+               "\nUtilized Budget: " + utilizedBudget +
+               "\nRemaining Budget: " + remainingBudget;
+    }
 }
+
