51eb4b13084542f727fdbbc372aa2b2936edf07e
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-21 21:12:06 +0500

    Update Rector.java

--- Project_OOP/src/Models/Rector.java
+++ Project_OOP/src/Models/Rector.java
@@ -1,96 +1,38 @@
 package Models;
 
-import java.util.List;
-import java.util.Objects;
-import javax.naming.InsufficientResourcesException;
-
 public class Rector extends Employee {
     private double universityBudget;
-    private List<String> accreditedPrograms;
 
-    public Rector(String username, String password, String userId, String nameFirst, String nameLast, String email, double budget, List<String> accreditedPrograms) {
-        super(username, password, userId, nameFirst, nameLast, email);
+    public Rector(String username, String userId, String firstName, String lastName, String email, double budget) {
+        super(username, userId, firstName, lastName, email);
         this.universityBudget = budget;
-        this.accreditedPrograms = accreditedPrograms;
     }
 
-    // Getters and Setters
     public double getUniversityBudget() {
         return universityBudget;
     }
 
-    public void setUniversityBudget(double universityBudget) {
-        this.universityBudget = universityBudget;
-    }
-
-    public List<String> getAccreditedPrograms() {
-        return accreditedPrograms;
-    }
-
-    public void setAccreditedPrograms(List<String> accreditedPrograms) {
-        this.accreditedPrograms = accreditedPrograms;
-    }
-
-    // Budget Management
-    public void manageBudget(double amount) throws InsufficientResourcesException {
+    public void manageBudget(double amount) throws IllegalArgumentException {
         if (amount < 0 && Math.abs(amount) > universityBudget) {
-            throw new InsufficientResourcesException("Not enough budget for this operation!");
+            throw new IllegalArgumentException("Not enough budget to perform this operation!");
         }
         universityBudget += amount;
+        System.out.println("Budget updated. Current budget: " + universityBudget);
     }
 
-    // Program Management
-    public void approveProgram(String program) {
-        accreditedPrograms.add(program);
-        System.out.println("Program approved: " + program);
+    public void approveDeanDecision(String decision) {
+        System.out.println("Rector " + getFirstName() + " approved the dean's decision: " + decision);
     }
 
-    public void revokeProgram(String program) {
-        accreditedPrograms.remove(program);
-        System.out.println("Program revoked: " + program);
+    public void allocateFunding(String facultyName, double amount) throws IllegalArgumentException {
+        if (amount > universityBudget) {
+            throw new IllegalArgumentException("Not enough budget to allocate " + amount + " to " + facultyName);
+        }
+        universityBudget -= amount;
+        System.out.println("Allocated " + amount + " to " + facultyName + ". Remaining budget: " + universityBudget);
     }
 
-    // Dean Management
-    public void appointDean(Dean dean, String faculty) {
-        System.out.println("Appointed Dean " + dean.getNameFirst() + " " + dean.getNameLast() + " to faculty: " + faculty);
-    }
-
-    // Research Projects
-    public void overseeResearchProjects(List<ResearchProject> projects) {
-        projects.forEach(project -> System.out.println("Overseeing research project: " + project.getTitle()));
-    }
-
-    // Address Issues
-    public void addressUniversityIssues(String issue) {
-        System.out.println("Resolving university issue: " + issue);
-    }
-
-    // Document Signin
-    public void signDocument(String document) {
-        System.out.println("Document signed by Rector: " + document);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Rector rector = (Rector) obj;
-        return Double.compare(rector.universityBudget, universityBudget) == 0 &&
-               Objects.equals(accreditedPrograms, rector.accreditedPrograms) &&
-               super.equals(obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(universityBudget, accreditedPrograms, super.hashCode());
-    }
-
-    @Override
-    public String toString() {
-        return "Rector{" +
-               "universityBudget=" + universityBudget +
-               ", accreditedPrograms=" + accreditedPrograms +
-               ", " + super.toString() +
-               '}';
+    public void signAgreement(String agreementName) {
+        System.out.println("Rector " + getFirstName() + " signed the agreement: " + agreementName);
     }
 }
