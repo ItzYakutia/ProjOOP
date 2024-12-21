765ade9ca903aea9084fce8e2832c15f0da203f5
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 01:06:10 +0500

    Update ResearchProject.java

--- Project_OOP/src/Models/ResearchProject.java
+++ Project_OOP/src/Models/ResearchProject.java
@@ -5,25 +5,53 @@ import java.util.List;
 
 public class ResearchProject {
     private String topic;
+    private List<ResearchPaper> publishedPapers;
     private List<Researcher> participants;
+    private Researcher supervisor;
 
-    public ResearchProject(String topic) {
+    public ResearchProject(String topic, Researcher supervisor) {
         this.topic = topic;
+        this.supervisor = supervisor;
+        this.publishedPapers = new ArrayList<>();
         this.participants = new ArrayList<>();
+        this.participants.add(supervisor); // Супервайзер автоматически добавляется
     }
 
     public String getTopic() {
         return topic;
     }
 
-    public void addParticipant(Researcher researcher) throws Exception {
-        if (!(researcher instanceof Researcher)) {
-            throw new Exception("Only researchers can be participants!");
-        }
-        participants.add(researcher);
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public List<ResearchPaper> getPublishedPapers() {
+        return publishedPapers;
     }
 
     public List<Researcher> getParticipants() {
         return participants;
+    }
+
+    public Researcher getSupervisor() {
+        return supervisor;
+    }
+
+    public void addResearchPaper(ResearchPaper paper) {
+        this.publishedPapers.add(paper);
+    }
+
+    public void addParticipant(Researcher researcher) {
+        if (researcher == null) {
+            throw new IllegalArgumentException("Only Researchers can participate in the project.");
+        }
+        this.participants.add(researcher);
+    }
+
+    public void approveDiplomaProject(String projectTitle, Researcher researcher) {
+        if (!participants.contains(researcher)) {
+            throw new IllegalArgumentException("Only participants of the project can submit a diploma project.");
+        }
+        System.out.println("Diploma project \"" + projectTitle + "\" approved for Researcher: " + researcher.getNameFirst() + " " + researcher.getNameLast());
     }
 }
