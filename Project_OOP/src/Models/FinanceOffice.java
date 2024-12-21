8437ee56ddc08bab37e3c261b05afec9fb1c4b1d
Yxk <69515436+ItzYakutia@users.noreply.github.com>
2024-12-22 05:41:17 +0900

    Update FinanceOffice.java

--- Project_OOP/src/Models/FinanceOffice.java
+++ Project_OOP/src/Models/FinanceOffice.java
@@ -7,7 +7,7 @@ public class FinanceOffice {
     private String officeName;
     private FinanceManager manager;
     private List<String> employees;
-    private Map<String, Double> employeeSalaries; 
+    private Map<String, Double> employeeSalaries;
 
     public FinanceOffice(String officeName, FinanceManager manager, List<String> employees, Map<String, Double> employeeSalaries) {
         this.officeName = officeName;
@@ -16,6 +16,51 @@ public class FinanceOffice {
         this.employeeSalaries = employeeSalaries;
     }
 
+    public boolean updateEmployeeSalary(String userId, double salary) {
+        if (employeeSalaries.containsKey(userId)) {
+            employeeSalaries.put(userId, salary);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean allocateFunds(int departmentId, double amount) {
+        System.out.println("Allocating funds to Department " + departmentId + ": $" + amount);
+        return true; // Simulate successful allocation
+    }
+
+    public String generateTransactionLog(List<Transaction> transactions) {
+        StringBuilder log = new StringBuilder("Transaction Log:\n");
+        for (Transaction transaction : transactions) {
+            log.append(transaction).append("\n");
+        }
+        return log.toString();
+    }
+
+    public String getSalaryDetails() {
+        StringBuilder details = new StringBuilder("Salary Details:\n");
+        for (Map.Entry<String, Double> entry : employeeSalaries.entrySet()) {
+            details.append("Employee ID: ").append(entry.getKey())
+                   .append(", Salary: $").append(entry.getValue()).append("\n");
+        }
+        return details.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder employeeSalariesString = new StringBuilder();
+        for (Map.Entry<String, Double> entry : employeeSalaries.entrySet()) {
+            employeeSalariesString.append("\nEmployee ID: ").append(entry.getKey())
+                                  .append(", Salary: $").append(entry.getValue());
+        }
+
+        return "Finance Office: " + officeName +
+               "\nManager: " + manager.getNameFirst() + " " + manager.getNameLast() +
+               "\nEmployees: " + employees +
+               "\nEmployee Salaries: " + employeeSalariesString;
+    }
+
+    // Getters and Setters
     public String getOfficeName() {
         return officeName;
     }
@@ -23,7 +68,6 @@ public class FinanceOffice {
     public void setOfficeName(String officeName) {
         this.officeName = officeName;
     }
-
 
     public FinanceManager getManager() {
         return manager;
@@ -47,19 +91,5 @@ public class FinanceOffice {
 
     public void setEmployeeSalaries(Map<String, Double> employeeSalaries) {
         this.employeeSalaries = employeeSalaries;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder employeeSalariesString = new StringBuilder();
-        for (Map.Entry<String, Double> entry : employeeSalaries.entrySet()) {
-            employeeSalariesString.append("\nEmployee ID: ").append(entry.getKey())
-                                  .append(", Salary: $").append(entry.getValue());
-        }
-        
-        return "Finance Office: " + officeName +
-               "\nManager: " + manager.getNameFirst() + " " + manager.getNameLast() +
-               "\nEmployees: " + employees +
-               "\nEmployee Salaries: " + employeeSalariesString;
     }
 }
