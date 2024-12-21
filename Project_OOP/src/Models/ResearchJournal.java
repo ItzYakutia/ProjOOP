8a021885edd91dd531f0d76f0faecde572470b6c
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-22 01:53:30 +0500

    Update ResearchJournal.java

--- Project_OOP/src/Models/ResearchJournal.java
+++ Project_OOP/src/Models/ResearchJournal.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResearchJournal {
-    private String name;
-    private List<User> subscribers; 
-    private List<ResearchPaper> publishedPapers;
+    private String name; // Название журнала
+    private List<User> subscribers; // Подписчики (любые пользователи)
+    private List<ResearchPaper> publishedPapers; // Опубликованные статьи
 
     public ResearchJournal(String name) {
         this.name = name;
@@ -22,21 +22,34 @@ public class ResearchJournal {
         return publishedPapers;
     }
 
+    // Подписка пользователя
     public void subscribe(User user) {
         if (!subscribers.contains(user)) {
             subscribers.add(user);
+            System.out.println(user.getNameFirst() + " subscribed to " + name);
+        } else {
+            System.out.println(user.getNameFirst() + " is already subscribed to " + name);
         }
     }
 
+    // Отписка пользователя
     public void unsubscribe(User user) {
-        subscribers.remove(user);
+        if (subscribers.contains(user)) {
+            subscribers.remove(user);
+            System.out.println(user.getNameFirst() + " unsubscribed from " + name);
+        } else {
+            System.out.println(user.getNameFirst() + " is not subscribed to " + name);
+        }
     }
 
+    // Публикация новой статьи
     public void publishPaper(ResearchPaper paper) {
         publishedPapers.add(paper);
+        System.out.println("New paper published in " + name + ": " + paper.getTitle());
         notifySubscribers(paper);
     }
 
+    // Уведомление всех подписчиков о новой статье
     private void notifySubscribers(ResearchPaper paper) {
         for (User subscriber : subscribers) {
             subscriber.receiveNotification("New paper published in " + name + ": " + paper.getTitle());
