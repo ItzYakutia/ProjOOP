ddaf14b8ab356a79bb1426033beeacb403abae9d
n0psw <108538929+n0psw@users.noreply.github.com>
2024-12-23 02:52:08 +0500

    Update Employee.java

--- Project_OOP/src/Models/Employee.java
+++ Project_OOP/src/Models/Employee.java
@@ -1,9 +1,20 @@
 package Models;
 
 public abstract class Employee extends User{
-
+	private Researcher researcherProfile;
 	public Employee(String username, String password, String userId, String email, String nameLast, String nameFirst) {
 		super(username, password, userId, email, nameLast, nameFirst);
 	}
+	public void becomeResearcher() {
+        if (researcherProfile == null) {
+            this.researcherProfile = new Researcher(getUsername(), getUserId(), getNameFirst(), getNameLast(), getEmail(), getPassword());
+            System.out.println("Employee " + getNameFirst() + " " + getNameLast() + " became a Researcher.");
+        } else {
+            System.out.println("Employee " + getNameFirst() + " " + getNameLast() + " is already a Researcher.");
+        }
+    }
 
+    	public Researcher getResearcherProfile() {
+        	return researcherProfile;
+    }
 }
